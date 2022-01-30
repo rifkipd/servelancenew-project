eb015b398a1ef21edbdcba31cb42e3f911338a3c
Rifki Padilah <rpadilah90@gmail.com>
2022-01-30 13:29:11 +0700

    fixing route

--- routes/web.php
+++ routes/web.php
@@ -31,10 +31,10 @@ use App\Http\Controllers\Dashboard\ProfileController;
 //route untuk landing
 //pemanggilan landing function yang berbeda dari resource harus diatasnya
 
-Route::get('detail_booking/{id}', LandingController::class, 'detail_booking')->name('detail.booking.landing');
-Route::get('booking/{id}', LandingController::class, 'booking')->name('booking.landing');
-Route::get('detail/{id}', LandingController::class, 'detail')->name('detail.landing');
-Route::get('explorer', LandingController::class, 'explorer')->name('explorer.landing');
+Route::get('detail_booking/{id}', [LandingController::class, 'detail_booking'])->name('detail.booking.landing');
+Route::get('booking/{id}', [LandingController::class, 'booking'])->name('booking.landing');
+Route::get('detail/{id}', [LandingController::class, 'detail'])->name('detail.landing');
+Route::get('explore', [LandingController::class, 'explore'])->name('explore.landing');
 Route::resource('/', LandingController::class);
 
 
@@ -54,20 +54,20 @@ Route::group(
 
 
         //request
-        Route::get('approve_request/{id}', RequestController::class, 'approve')->name('approve.request');
+        Route::get('approve_request/{id}', [RequestController::class, 'approve'])->name('approve.request');
         Route::resource('request', RequestController::class);
 
 
 
         //myorder
-        Route::get('accept/order/{id}', MyOrderController::class, 'accepted')->name('accept.order');
-        Route::get('reject/order/{id}', MyOrderController::class, 'rejected')->name('reject.order');
+        Route::get('accept/order/{id}', [MyOrderController::class, 'accepted'])->name('accept.order');
+        Route::get('reject/order/{id}', [MyOrderController::class, 'rejected'])->name('reject.order');
         Route::resource('order', MyOrderController::class);
 
 
 
         //profile
-        Route::get('delete_photo', ProfileController::class, 'delete')->name('delete.photo.profile');
+        Route::get('delete_photo', [ProfileController::class, 'delete'])->name('delete.photo.profile');
         Route::resource('profile', ProfileController::class);
     }
 );
